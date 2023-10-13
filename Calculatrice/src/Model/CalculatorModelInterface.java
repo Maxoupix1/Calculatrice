e4f93b25e6a2b5da9b8445049f5a27085cba62da
Maxime <Maxime@159.31.79.91>
2023-10-13 15:11:52 +0200

    Création de l'interface

---
+++ Calculatrice/src/Model/CalculatorModelInterface.java
@@ -0,0 +1,16 @@
+package Model;
+
+public interface CalculatorModelInterface {
+	public void add();
+	public void substract();
+	public void multiply();
+	public void divide();
+	public void opposite();
+	public void push();
+	public void pop();
+	public void drop();
+	public void swap();
+	public void clear();
+	
+
+}
